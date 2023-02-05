dae9c356bad432711f5cedf6dfaa7a651811b36f
Bot123s <82277200+Neang1@users.noreply.github.com>
2023-02-05 08:26:38 -0500

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_r8q.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_r8q-eng \
+    lineage_r8q-userdebug \
+    lineage_r8q-user
---
+++ BoardConfig.mk
@@ -0,0 +1,24 @@
+# Inherit common board flags
+include device/samsung/sm8250-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/r8q
+
+# Platform
+TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := r8q
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/r8q_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := vendor/variant_r8q_defconfig
+KERNEL_LD := DEBUG_DEFCONFIG=vendor/aosp_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
+# Display
+TARGET_SCREEN_DENSITY := 450
+
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
